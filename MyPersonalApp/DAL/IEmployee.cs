60dd5f212e5d908ddedd26cabbb0883df1746f8d
Arizza <androarizza@gmail.com>
2023-01-30 13:55:33 +0700

    Add Connection, Dal, Interface for DAL

---
+++ MyPersonalApp/DAL/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿using MyPersonalApp.Models;
+using System.Data;
+
+namespace MyPersonalApp.DAL
+
+{
+    public interface IEmployee
+    {
+        public IEnumerable<Employees>GetAll();
+        public Employees GetById(UniqueConstraint uniqueConstraint);        
+
+    }
+}
