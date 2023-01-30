25ac799fb72af871076ab56e6d4a610fc7ffe1e1
Arizza <androarizza@gmail.com>
2023-01-30 11:48:43 +0700

    + Models

---
+++ MyPersonalApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPersonalApp.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; } 
+        public string City { get; set; }
+        public string Region { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
