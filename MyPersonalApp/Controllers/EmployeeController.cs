3ef9f3569e459b9d89ccd09d7153f45ec521874e
Rapidtech <Rapidtech@LAPTOP-G4BIJ8K2>
2023-01-30 11:05:33 +0700

    Add project files.

---
+++ MyPersonalApp/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPersonalApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "This is list of employee";
+        }
+    }
+}
