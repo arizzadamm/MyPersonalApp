a3c1072343698af8c9329bceb27776c4d248baf3
Arizza <androarizza@gmail.com>
2023-01-31 15:20:08 +0700

    Test EF

---
+++ MyPersonalApp/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPersonalApp.Models;
+
+namespace MyPersonalApp.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Employees> Employees { get; set; }  
+        public DbSet<Position> Positions { get; set; }  
+    }
+}
---
+++ MyPersonalApp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace MyPersonalApp.Models
+{
+    public class Employees
+    {
+        public Guid Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int Phone { get; set; }
+        public int PositionId { get; set; } 
+        public string? Address { get; set; } 
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public int PostalCode { get; set; }
+        public string? Country { get; set; }
+        public List<Position>? Positions { get; set; }   
+    }
+}
